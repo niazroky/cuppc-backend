199746ba166def9d0979ff8a0a23e1d205153f3f
Md. Niazul Islam Roky <niazroky75@gmail.com>
2025-01-17 15:42:24 +0600

    Update settings.py

--- cuppc_backend/settings.py
+++ cuppc_backend/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-xmr^gkzos*ih@23wmo&@%%#tfek854yx3kcmflxdtk6c!22cx7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['98.83.30.169', 'localhost']
 
 
 # Application definition
